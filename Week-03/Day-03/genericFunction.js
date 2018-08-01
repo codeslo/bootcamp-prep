01f591d4a50cd1489633040e0c14284432284dfd
stillmattwest <stillmattwest@gmail.com>
2018-08-01 15:59:21 -0700

    changed source code folders

---
+++ Week-03/Day-03/genericFunction.js
@@ -0,0 +1,8 @@
+// every coding challenge answer is going to have this format
+
+// every function has a declaration, a name, and a body. Some accept parameters. If you have multiple parameters, they are separated by commas.
+
+function yourFunctionName(param1,param2,param3){
+    // this is the function body
+    return // whatever you need to return
+}
---
+++ Week-03/Day-04/indexOf.js
@@ -0,0 +1,7 @@
+let arr = [1,2,3,4,5,6];
+
+let value = 3;
+
+let pos = arr.indexOf(value);
+
+console.log(pos);
