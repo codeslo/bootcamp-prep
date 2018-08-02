af11ce8474bbb5f1496e5730f346aa3519b95450
stillmattwest <stillmattwest@gmail.com>
2018-08-02 15:18:58 -0700

    added and moved source code to appropriate weeks / days

--- Week-03/Day-04/indexOf.js
+++
@@ -1,7 +0,0 @@
-let arr = [1,2,3,4,5,6];
-
-let value = 3;
-
-let pos = arr.indexOf(value);
-
-console.log(pos);
--- Week-03/Day-04/onlyOdds.js
+++ Week-03/Day-04/onlyOdds.js
@@ -1,7 +1,7 @@
 let result = [];
 
 for(let i = 0; i < 1000; i++){
-    if(i % 2){
+    if(i % 2 !== 0){
         result.push(i)
     }
 }
