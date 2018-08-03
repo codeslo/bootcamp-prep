61d57d46cf3bc6312e49c5f765ec4a170f6c7e3e
stillmattwest <stillmattwest@gmail.com>
2018-08-03 10:17:49 -0700

    added class examples

--- Week-03/Day-04/onlyOdds.js
+++ Week-03/Day-04/onlyOdds.js
@@ -6,4 +6,17 @@ for(let i = 0; i < 1000; i++){
     }
 }
 
-console.log(result);
+//console.log(result);
+
+console.log(result.length);
+let newResult = [];
+for(let i = 0; i < result.length; i++){
+    if (result[i] >=30 && result[i]<=70){
+        newResult.push(result[i]);
+    }
+}
+console.log(newResult);
+
+let lastTen = result.splice(result.length - 10);
+
+console.log(lastTen);
